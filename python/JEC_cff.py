1fe1381d98b9e9078ad5f0bf5ff76494c4bb018a
Jorge Chaves <jec429@cornell.edu>
2015-11-02 08:24:49 -0600

    JEC updated

--- python/JEC_cff.py
+++ python/JEC_cff.py
@@ -1,5 +1,5 @@
 
-def JEC_correction(process):
+def JEC_correction_data(process):
     process.load("CondCore.DBCommon.CondDBCommon_cfi")
     from CondCore.DBCommon.CondDBSetup_cfi import *
     process.jec = cms.ESSource("PoolDBESSource",
@@ -10,14 +10,14 @@ def JEC_correction(process):
         toGet = cms.VPSet(
         cms.PSet(
             record = cms.string('JetCorrectionsRecord'),
-            tag    = cms.string('JetCorrectorParametersCollection_Summer15_50nsV2_MC_AK4PFchs'),
+            tag    = cms.string('JetCorrectorParametersCollection_Summer15_25nsV5_DATA_AK4PFchs'),
                 # tag    = cms.string('JetCorrectorParametersCollection_Summer12_V3_MC_AK5PF'),
             label  = cms.untracked.string('AK4PFchs')
             ),
         ## here you add as many jet types as you need
         ## note that the tag name is specific for the particular sqlite file 
         ), 
-        connect = cms.string('sqlite:Summer15_50nsV2_MC.db')
+        connect = cms.string('sqlite:Summer15_25nsV5_DATA.db')
     )
     ## add an es_prefer statement to resolve a possible conflict from simultaneous connection to a global tag
     process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
@@ -25,14 +25,34 @@ def JEC_correction(process):
     
     from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetCorrFactorsUpdated
     process.patJetCorrFactorsReapplyJEC = patJetCorrFactorsUpdated.clone(
-    src = cms.InputTag("slimmedJets"),
-    levels = cms.vstring('L1FastJet', 
+        src = cms.InputTag("slimmedJets"),
+        levels = cms.vstring('L1FastJet', 
             'L2Relative', 
             'L3Absolute'),
-    payload = cms.string('AK4PFchs') ) # Make sure to choose the appropriate levels and payload here!
+        payload = cms.string('AK4PFchs') ) # Make sure to choose the appropriate levels and payload here!
     
     from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetsUpdated
     process.patJetsReapplyJEC = patJetsUpdated.clone(
-    jetSource = cms.InputTag("slimmedJets"),
-    jetCorrFactorsSource = cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
-  )
+        jetSource = cms.InputTag("slimmedJets"),
+        jetCorrFactorsSource = cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
+    )
+
+def JEC_correction_MC(process):
+    import FWCore.ParameterSet.Config as cms
+    process.load('Configuration.StandardSequences.Services_cff')
+    process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+    process.GlobalTag.globaltag = '74X_mcRun2_asymptotic_v2'
+    
+    from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetCorrFactorsUpdated
+    process.patJetCorrFactorsReapplyJEC = patJetCorrFactorsUpdated.clone(
+        src = cms.InputTag("slimmedJets"),
+        levels = cms.vstring('L1FastJet', 
+            'L2Relative', 
+            'L3Absolute'),
+        payload = cms.string('AK4PFchs') ) # Make sure to choose the appropriate levels and payload here!
+    
+    from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetsUpdated
+    process.patJetsReapplyJEC = patJetsUpdated.clone(
+        jetSource = cms.InputTag("slimmedJets"),
+        jetCorrFactorsSource = cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
+    )
